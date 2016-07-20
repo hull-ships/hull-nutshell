cb995268af07bb294004bc970a2b36ab2b2a4605
Stephane Bellity <sbellity@gmail.com>
2016-07-20 13:48:02 +0200

    Add support for batch and templates

--- server/index.js
+++ server/index.js
@@ -1,23 +1,30 @@
 import express from 'express';
 import path from 'path';
-import { NotifHandler } from 'hull';
+import { NotifHandler, BatchHandler } from 'hull';
 
 import updateUser from './update-user';
 
-export function Server() {
-
-  const notifHandler = NotifHandler({
-    groupTraits: false,
-    events: {
-      'user_report:update' : updateUser
-    }
-  });
+export function Server({ hostSecret = '123' }) {
 
   const app = express();
   app.use(express.static(path.resolve(__dirname, '..', 'dist')));
   app.use(express.static(path.resolve(__dirname, '..', 'assets')));
 
-  app.post('/notify', notifHandler);
+  app.post('/notify', NotifHandler({
+    hostSecret,
+    groupTraits: false,
+    handlers: {
+      'user:update': updateUser
+    }
+  }));
+
+  app.post('/batch', BatchHandler({
+    hostSecret,
+    groupTraits: false,
+    handler(notifications, context) {
+      return notifications.map(n => updateUser(n, context))
+    }
+  }));
 
   app.get('/manifest.json', (req, res) => {
     res.sendFile(path.resolve(__dirname, '..', 'manifest.json'));
--- server/update-user.js
+++ server/update-user.js
@@ -1,23 +1,23 @@
 import _ from 'lodash';
 import request from 'request';
-import objectMapper from 'object-mapper';
+import Hogan from 'hogan.js';
 
-const NUTSHELL_CREATED_AT = 'nutshell/created_at';
+export default function updateUser({ message={} }, { ship={}, hull }) {
 
-
-export default function({ message={} }, { ship={}, hull }) {
+  console.warn("Hello updateUser", message);
 
   const { user={}, segments=[] } = message;
 
+
   if (!ship || !user || !user.id) {
-    console.warn("Skip update: who is this", { user, ship })
+    console.warn("Bye bye")
     return false;
   }
 
 
   // User has already been pushed to nutshell
-  if (user[`traits_${NUTSHELL_CREATED_AT}`]) {
-    console.warn("Skip update: user already imported", user.id)
+  if (user[`traits_nutshell/created_at`]) {
+    console.warn("Skip update: user already imported", { id: user.id, email: user.email });
     return false;
   }
 
@@ -33,10 +33,9 @@ export default function({ message={} }, { ship={}, hull }) {
     synchronized_segments.length > 0 &&
     !_.intersection(_.map(segments, 'id'), synchronized_segments).length
     ) {
-    console.log(`Skip update for ${user.id} because not matching any segment`);
+    console.warn(`Skip update for ${user.id} because not matching any segment`);
     return false;
-  };
-
+  }
 
   // Ignore if mapping is not defined
   if (!mapping || !mapping.length) {
@@ -44,35 +43,37 @@ export default function({ message={} }, { ship={}, hull }) {
     return false;
   }
 
-  let missingField;
-  const userMapping = mapping.reduce((r,m) => {
-    if (r && (user[m.hull] || !m.is_required)) {
-      r[m.hull] = {
-        key: m.nutshell,
-        transform: (val) => _.isArray(val) ? val.join(", ") : val
-      };
-    } else {
-      missingField = m;
-      r = false
+
+  let missingField = false;
+  const form = mapping.reduce((r, m) => {
+
+    if (r) {
+      let value;
+      try {
+        value = Hogan.compile(m.hull).render(user);
+      } catch (err) {
+        console.warn("Error in hogan render ", err);
+      }
+
+      if (_.isEmpty(value) && m.is_required) {
+        missingField = m;
+        return false;
+      }
+
+      return _.set(r, m.nutshell, value);
     }
-    return r;
   }, {});
 
-  if (!userMapping) {
+  if (!form) {
     console.log('Skip update:  missing required field: ', { missingField, user });
     return false;
-  }
-
-  const form = objectMapper(user, userMapping);
-  const traits = {
-    [NUTSHELL_CREATED_AT]: new Date().toISOString()
   }
 
   console.warn("Create user", user.id, JSON.stringify({form }));
 
   request.post({ url: form_api_url, form }, (err, res, body) => {
     if (!err && res.statusCode < 400) {
-      return hull.as(user.id).traits(traits);
+      return hull.as(user.id).traits({ created_at: new Date().toISOString() }, { group: 'nutshell' });
     }
   });
 
